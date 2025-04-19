36371a74a305d11f2bb835850797c8ee226c4991
AUNT1oN256 <anton.mironets@gmail.com>
2025-04-19 15:09:12 +0300

    add 3d cube and move

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
+import { createRoot  } from 'react-dom/client';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { useRef } from 'react';
+import { OrbitControls } from '@react-three/drei';
+
 import './App.css';
 import React from "react";
 
+function RotatingBox() {
+  const meshRef = useRef();
+
+
+  useFrame(() => {
+    if (meshRef.current){
+      meshRef.current.rotation.x += 0;
+      meshRef.current.rotation.y += 0;
+    }
+  });
+  
+  return(
+    <mesh ref={meshRef}>
+      <boxGeometry args={[5, 5, 5]}/>
+      <meshStandardMaterial color="yellow"/>
+    </mesh>
+  );
+
+}
+
 function App() {
   return (
-    <div className="App">
-      <h1>React Template</h1>
-      <p>Start template React-projects</p>
+    <div id="canvas-container" className="App">
+      <Canvas>
+        <RotatingBox />
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <OrbitControls />
+      </Canvas>
     </div>
   );
 }
 
+createRoot(document.getElementById('root')).render(<App />);
 export default App;
